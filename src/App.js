8e505e0be9c6efb316e4d9e99292bad8001f4607
Patricia Muresan <patriciamariamrsn@gmail.com>
2024-02-02 16:35:59 -0500

    added the hooks to fetch the data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import logo from './logo.svg';
+import { useEffect, useState } from "react";
+import CardData from './components/Card';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+  const [charData, setCharData] = useState([]);
+
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch("https://rickandmortyapi.com/api/character");
+      const data = await res.json();
+      console.log(data)
+
+      setCharData(data.results)
+    }
+    fetchData()
+  },[])
+
+
   return (
-    <div className="App">
-      
+    <div>
+      <CardData charData={charData} />
     </div>
   );
 }
